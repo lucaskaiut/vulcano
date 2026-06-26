386adef020696cb01da3871e4c26c6a41c573d33
Lucas Kaiut <lucas.kaiut@gmail.com>
2026-06-26 16:33:25 -0300

    fix: corrige tag </TableBody> removida acidentalmente na auditoria

--- web/src/pages/AuditLogsPage.tsx
+++ web/src/pages/AuditLogsPage.tsx
@@ -154,6 +154,7 @@ export function AuditLogsPage() {
                     </React.Fragment>
                   )
                 })}
+              </TableBody>
             </Table>
           </div>
 
